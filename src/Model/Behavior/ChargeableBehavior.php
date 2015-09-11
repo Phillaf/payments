2375489f76f09213df4ee0acbd44a4f86ccf2790
Philippe Lafrance <phil.laf@gmail.com>
2015-09-11 00:22:04 -0400

    Missing semicolumn
    also -> doc-block formatting

--- src/Model/Behavior/ChargeableBehavior.php
+++ src/Model/Behavior/ChargeableBehavior.php
@@ -10,7 +10,7 @@ use Cake\ORM\TableRegistry;
 use Cake\Network\Exception\InternalErrorException;
 use Cake\Core\Configure;
 use Cake\Utility\Hash;
-use Cake\Datasource\EntityInterface
+use Cake\Datasource\EntityInterface;
 
 /**
  * Chargeable Behavior
@@ -33,11 +33,12 @@ class ChargeableBehavior extends Behavior
         'defaultDescription' => 'Item Description'
     ];
     
-    /* Initialize the chargeable behavior
-    *
-    * @param array $config
-    * @return void
-    */
+    /** 
+     * Initialize the chargeable behavior
+     *
+     * @param array $config
+     * @return void
+     */
     public function initialize(array $config)
     {
         if (is_null($config['amount'])) {
@@ -45,14 +46,15 @@ class ChargeableBehavior extends Behavior
         }
     }
     
-    /* Purchase a chargeable item with the configurated payment gateway
-    *
-    * @param array $cardData
-    * @param integer $userId
-    * @param integer $chargeableId
-    * @param interger $quantity
-    * @return \Payments\Entity\Charge;
-    */
+    /**
+     * Purchase a chargeable item with the configurated payment gateway
+     *
+     * @param array $cardData
+     * @param integer $userId
+     * @param integer $chargeableId
+     * @param interger $quantity
+     * @return \Payments\Entity\Charge;
+     */
     public function purchase(array $cardData, $userId, $chargeableId, $quantity)
     {
         // todo: deal with gateway config somewhere else
@@ -83,12 +85,13 @@ class ChargeableBehavior extends Behavior
         return $charge;
     }
     
-    /* Set the fields of the desired chargeable objects. If any field does
-    * not exist in the database, the default configuration value is used.
-    *
-    * @param \Cake\Datasource\EntityInterface $chargeable
-    * @return \Cake\Datasource\EntityInterface
-    */
+    /**
+     * Set the fields of the desired chargeable objects. If any field does
+     * not exist in the database, the default configuration value is used.
+     *
+     * @param \Cake\Datasource\EntityInterface $chargeable
+     * @return \Cake\Datasource\EntityInterface
+     */
     protected function setFields(EntityInterface $chargeable)
     {   
         // Set necessary fields
