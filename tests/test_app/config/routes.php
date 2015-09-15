b024920ea82edb7ec6dfde5edde30b1283533678
Philippe Lafrance <phil.laf@gmail.com>
2015-09-15 16:04:24 -0400

    This logic will be held in the TestCases and not the sample app.

--- tests/test_app/config/routes.php
+++ tests/test_app/config/routes.php
@@ -4,8 +4,8 @@ use Cake\Core\Plugin;
 use Cake\Routing\Router;
 
 Router::scope('/', function ($routes) {
-  $routes->extensions(['json']);
-  $routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'InflectedRoute']);
-  $routes->connect('/:controller/:action/*', [], ['routeClass' => 'InflectedRoute']);
+    $routes->extensions(['json']);
+    $routes->connect('/:controller', ['action' => 'index'], ['routeClass' => 'InflectedRoute']);
+    $routes->connect('/:controller/:action/*', [], ['routeClass' => 'InflectedRoute']);
 });
 Plugin::routes();
